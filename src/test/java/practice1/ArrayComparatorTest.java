2df3f176addbde8750ac214cb63305afe38792e3
Mark Solonchev <solonchev32@gmail.com>
2024-02-16 21:45:57 +0300

    Added tests for practice 1

---
+++ src/test/java/practice1/ArrayComparatorTest.java
@@ -0,0 +1,46 @@
+package practice1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import practice1.main.ArrayComparator;
+
+public class ArrayComparatorTest {
+
+    @Test
+    public void FirstSmallerArrayShouldReturnNegativeValue() {
+        ArrayComparator<String> comparator = new ArrayComparator<>();
+        String[] stringArray1 = {"Hello", "world"};
+        String[] stringArray2 = {"Hello", "world", "!"};
+
+        int actualResponse = comparator.compare(stringArray1, stringArray2);
+        int expectedResponse = -1;
+
+        Assertions.assertEquals(actualResponse, expectedResponse);
+    }
+
+    @Test
+    public void SecondSmallerArrayShouldReturnPositiveValue() {
+        ArrayComparator<String> comparator = new ArrayComparator<>();
+        String[] stringArray1 = {"Hello", "world", "!"};
+        String[] stringArray2 = {"Hello", "world"};
+
+        int actualResponse = comparator.compare(stringArray1, stringArray2);
+        int expectedResponse = 1;
+
+        Assertions.assertEquals(actualResponse, expectedResponse);
+    }
+
+    @Test
+    public void ArraysWithSameLengthShouldReturnZero() {
+        ArrayComparator<String> comparator = new ArrayComparator<>();
+        String[] stringArray1 = {"Hello", "world", "!"};
+        String[] stringArray2 = {"Hello", "world", "!"};
+
+        int actualResponse = comparator.compare(stringArray1, stringArray2);
+        int expectedResponse = 0;
+
+        Assertions.assertEquals(actualResponse, expectedResponse);
+    }
+
+
+}
