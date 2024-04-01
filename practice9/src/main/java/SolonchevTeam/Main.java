2f503b5f1c7c6bf0da14aa75a21c3b61f15c3389
Mark Solonchev <solonchev32@gmail.com>
2024-04-01 16:14:49 +0300

    practice9

---
+++ practice9/src/main/java/SolonchevTeam/Main.java
@@ -0,0 +1,7 @@
+package SolonchevTeam;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world from Gradle Project!");
+    }
+}
--- spring/src/main/java/ru/solonchev/spring/Application.java
+++ spring/src/main/java/ru/solonchev/spring/Application.java
@@ -2,16 +2,10 @@ package ru.solonchev.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.solonchev.spring.practice10.test.AlgorithmTest;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
-        var context = SpringApplication.run(Application.class, args);
-        // PRACTICE 10
-//        var test = context.getBean(AlgorithmTest.class);
-//        test.test();
+        SpringApplication.run(Application.class, args);
     }
-
 }
