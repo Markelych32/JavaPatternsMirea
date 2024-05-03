7865a847c386c1d3c51bc5055404deebae12bde9
Mark Solonchev <solonchev32@gmail.com>
2024-05-03 13:03:58 +0300

    add practice 20

---
+++ practice15/src/main/java/ru/solonchev/practice15/aspects/LoggingAspect.java
@@ -0,0 +1,22 @@
+package ru.solonchev.practice15.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect
+public class LoggingAspect {
+    @Before("allServiceMethods()")
+    public void logParameters(JoinPoint joinPoint) {
+        log.info("Parameters: {}", joinPoint.getArgs());
+    }
+
+    @Pointcut("within(ru.solonchev.practice15.service.*)")
+    public void allServiceMethods() {
+    }
+}
